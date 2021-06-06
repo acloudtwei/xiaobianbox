aea72e6292a6cd40e7275c7506fd6c0d154cac80
acloud <1559295172@qq.com>
2021-06-06 20:41:32 +0800

    小部分更新

--- app/src/main/java/com/example/one/MainActivity.java
+++ app/src/main/java/com/example/one/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import com.loopj.android.image.SmartImageView;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,14 +24,17 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
     private TextView texts;
+    private SmartImageView bg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         texts = (TextView) findViewById(R.id.text);
+        bg = (SmartImageView) findViewById(R.id.bgs);
         getAsyn("https://restapi.amap.com/v3/weather/weatherInfo?key=07ce7c4969a77c908df5bc177601983d&city=河源");
         SmartImageView images = (SmartImageView) findViewById(R.id.abc);
         images.setImageUrl("https://tva2.sinaimg.cn/large/0072Vf1pgy1foxk744kw6j31hc0u0ni5.jpg");
+//        getbg("https://api.dongmanxingkong.com/suijitupian/acg/1080p/index.php?return=json");
     }
     public void getAsyn(String url) {
         OkHttpClient client = new OkHttpClient();
@@ -47,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 if(response.isSuccessful()){
-                    //                        JSONObject jsonobject = new JSONObject(response.body().string());
+//                        JSONObject jsonobject = new JSONObject(response.body().string());
 //                        JSONArray jsonarray =jsonobject.getJSONArray("lives");
 //                        JSONObject jsonobjects = jsonarray.getJSONObject(0);
 //                        String count = jsonobjects.optString("weather");
@@ -57,6 +61,45 @@ public class MainActivity extends AppCompatActivity {
                     //处理UI需要切换到UI线程处理
 
                 }
+                else
+                {
+                    texts.setText("请求失败");
+                }
+            }
+        });
+    }
+    public void getbg(String url) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).get().build();
+        Call call = client.newCall(request);
+        // 创建call对象，这个Call是用来执行request请求的，就是说我们创建了一个请求，但是执行使用的是call，有同步请求和异步请求
+//        try {
+//            Response response = call.execute();
+//            try {
+//                JSONObject jsonobject = new JSONObject(response.body().string());
+//                bg.setImageUrl(jsonobject.optString("imgurl"));
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    JSONObject jsonobject = null;
+                    try {
+                        jsonobject = new JSONObject(response.body().string());
+                        Log.d("aa",jsonobject.optString("imgurl"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("异常", "请求失败，可能是服务器不存在");
             }
         });
     }
