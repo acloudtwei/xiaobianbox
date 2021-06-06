05c3d4d7d2b74558fe2c3c50712ad4d64b713ba8
副机 <1405810807@qq.com>
2021-06-07 01:13:58 +0800

    新增了bomb后端云！

--- app/src/main/java/com/example/one/MainActivity.java
+++ app/src/main/java/com/example/one/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.loopj.android.image.SmartImageView;
 
 import org.jetbrains.annotations.NotNull;
@@ -14,7 +16,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
 
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.datatype.BmobQueryResult;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.QueryListener;
+import cn.bmob.v3.listener.SQLQueryListener;
+import cn.bmob.v3.listener.SaveListener;
+import cn.bmob.v3.listener.UpdateListener;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -28,14 +39,93 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bmob.initialize(this, "83e23941491c7cce0cda92a4fdfe54c8");
         setContentView(R.layout.activity_main);
-        texts = (TextView) findViewById(R.id.text);
-        bg = (SmartImageView) findViewById(R.id.bgs);
-        getAsyn("https://restapi.amap.com/v3/weather/weatherInfo?key=07ce7c4969a77c908df5bc177601983d&city=河源");
-        SmartImageView images = (SmartImageView) findViewById(R.id.abc);
-        images.setImageUrl("https://tva2.sinaimg.cn/large/0072Vf1pgy1foxk744kw6j31hc0u0ni5.jpg");
+//        update();
+          query();
+//        person p2 = new person();
+//        p2.setNum(1);
+//        p2.setName("lucky");
+//        p2.setAddress("北京海淀");
+//        p2.save(new SaveListener<String>() {
+//            @Override
+//            public void done(String objectId, BmobException e) {
+//                if(e==null){
+//                    Toast.makeText(getApplication(),"添加数据成功，返回objectId为："+objectId,Toast.LENGTH_SHORT).show();
+//                }else{
+//                    Toast.makeText(getApplication(),"=失败："+e.getMessage(),Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//        texts = (TextView) findViewById(R.id.text);
+//        bg = (SmartImageView) findViewById(R.id.bgs);
+//        getAsyn("https://restapi.amap.com/v3/weather/weatherInfo?key=07ce7c4969a77c908df5bc177601983d&city=河源");
+//        SmartImageView images = (SmartImageView) findViewById(R.id.abc);
+//        images.setImageUrl("https://tva2.sinaimg.cn/large/0072Vf1pgy1foxk744kw6j31hc0u0ni5.jpg");
 //        getbg("https://api.dongmanxingkong.com/suijitupian/acg/1080p/index.php?return=json");
     }
+    private void update() {
+        person category = new person();
+        category.setName("威威");
+        category.update("1ca2e55f8e", new UpdateListener() {
+            @Override
+            public void done(BmobException e) {
+                if (e == null) {
+                    Toast.makeText(getApplication(), "更新成功", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText( getApplication(),"更新失败", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void query() {
+        String sql = "select * from person";
+        BmobQuery<person> bmobQuery = new BmobQuery<>();
+        bmobQuery.setSQL(sql);
+        bmobQuery.doSQLQuery(new SQLQueryListener<person>() {
+            @Override
+            public void done(BmobQueryResult<person> bmobQueryResult, BmobException e) {
+                if (e == null) {
+                    List<person> list = (List<person>) bmobQueryResult.getResults();
+                    Toast.makeText(getApplication(), "查询成功：" + list.get(0).getName(), Toast.LENGTH_LONG).show();
+                } else {
+                    Log.e("BMOB", e.toString());
+                    Toast.makeText(getApplication(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+//        bmobQuery.getObject("", new QueryListener<person>() {
+//            @Override
+//            public void done(person persons, BmobException e) {
+//                if (e == null) {
+//                    Toast.makeText(getApplication(), "查询成功：" + persons.getName(), Toast.LENGTH_LONG).show();
+//                } else {
+//                    Log.e("BMOB", e.toString());
+//                    Toast.makeText(getApplication(), e.getMessage(), Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public void getAsyn(String url) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(url).build();
