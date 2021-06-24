0f147d55093333f389d0ff6faf6a990a3685f275
“小华硕” <aiyunkj@gmail.com>
2021-06-25 01:37:50 +0800

    已经完成了图片返回的api，但是无法显示出来，应该和http请求线程那方面有关

--- app/src/main/java/com/example/one/function/function3.java
+++ app/src/main/java/com/example/one/function/function3.java
@@ -2,6 +2,7 @@ package com.example.one.function;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.one.beans.*;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
@@ -26,6 +27,11 @@ import com.githang.statusbar.StatusBarCompat;
 import com.liji.imagezoom.util.ImageUrlType;
 import com.liji.imagezoom.util.ImageZoom;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,10 +41,14 @@ import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.datatype.BmobQueryResult;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.SQLQueryListener;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class function3 extends AppCompatActivity {
 
-    private ImageView tests;
     private EditText qcode_text;
     private TextView size2;
     private TextView margin2;
@@ -47,7 +57,7 @@ public class function3 extends AppCompatActivity {
     private Button qcode_create;
     private NumberPicker num_choose1;
     private NumberPicker num_choose2;
-
+    function3_bean function3_bean = new function3_bean();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +69,7 @@ public class function3 extends AppCompatActivity {
         initData2();
         iniChoose1();
         iniChoose2();
+        initData3();
 //        NumberPicker numberPicker = (NumberPicker) findViewById(R.id.ppcv);
 //        numberPicker.setOnValueChangeListener(new OnValueChangeListener() {
 //            @Override
@@ -66,29 +77,6 @@ public class function3 extends AppCompatActivity {
 //                Toast.makeText(function3.this,"ok"+newValue,Toast.LENGTH_SHORT).show();
 //            }
 //        });
-
-
-//        tests = (ImageView) findViewById(R.id.aca);
-//        final String[] urls = new String[] {
-//                "https://xiaojieapi.com/api/v3/get/picture?token=c1d5c801320489e792112c461819fe3b&suffix=jpg&type=jpg",
-//                "https://xiaojieapi.com/api/v3/get/picture?token=b05a7512d42fc3484e4cf3cfaf1cd11b&suffix=jpg&type=jpg",
-//        };
-//
-//        //添加list数据
-//        final List<String> list = new ArrayList<>();
-//        for (int i = 0; i < urls.length; i++) {
-//            list.add(urls[i]);
-//        }
-//
-//        //打开图集
-//        Button btn = (Button) findViewById(R.id.aca);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                ImageZoom.show(function3.this, urls[1], list);
-//            }
-//        });
-
 
 
 //取色器的监听器
@@ -146,6 +134,7 @@ public class function3 extends AppCompatActivity {
                     }
                     @Override
                     public void onColorConfirm(ColorPickerDialog dialog, int color) {//确定的颜色
+                        function3_bean.setBgcolor("%23"+Integer.toHexString(color).substring(2));
                         color_choose1.setBackgroundColor(color);
                         color_choose1.setText("#"+Integer.toHexString(color).substring(2));
                         //Toast.makeText(function3.this,"#"+Integer.toHexString(color),Toast.LENGTH_SHORT).show();
@@ -187,6 +176,7 @@ public class function3 extends AppCompatActivity {
                     public void onColorConfirm(ColorPickerDialog dialog, int color) {//确定的颜色
                         color_choose2.setBackgroundColor(color);
                         color_choose2.setText("#"+Integer.toHexString(color).substring(2));
+                        function3_bean.setFgcolor("%23"+Integer.toHexString(color).substring(2));
                         //Toast.makeText(function3.this,"#"+Integer.toHexString(color),Toast.LENGTH_SHORT).show();
                     }
                 };
@@ -200,12 +190,67 @@ public class function3 extends AppCompatActivity {
         });
     }
 
+    private void initData3()
+    {
+//        final String[] urls = new String[] {
+//                "https://xiaojieapi.com/api/v3/get/picture?token=c1d5c801320489e792112c461819fe3b&suffix=jpg&type=jpg",
+//                "https://xiaojieapi.com/api/v3/get/picture?token=b05a7512d42fc3484e4cf3cfaf1cd11b&suffix=jpg&type=jpg",
+//        };
+//
+//        //添加list数据
+//        final List<String> list = new ArrayList<>();
+//        for (int i = 0; i < urls.length; i++) {
+//            list.add(urls[i]);
+//        }
+        qcode_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = qcode_text.getText().toString().trim();
+                String url = "https://api.itwei.top/get_qcode.php?text="+text+"$bgcolor="+function3_bean.getBgcolor() +
+                        "&fgcolor="+function3_bean.getFgcolor()+"&w="+function3_bean.getW()+"&m="+function3_bean.getM();
+
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder().url(url).get().build();
+                Call call = client.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        ImageZoom.show(function3.this,"https://tva4.sinaimg.cn//large//0072Vf1pgy1foxlncsm2pj31hc0u0arz.jpg");
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        final String responseString = response.body().string();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(function3.this,url,Toast.LENGTH_LONG).show();
+                                try {
+                                    JSONObject jsonobject = new JSONObject(responseString);
+                                    if(jsonobject.optString("code").equals("1"))
+                                    {
+                                        //qcode_text.setText(jsonobject.optString("url"));
+                                        ImageZoom.show(function3.this,jsonobject.optString("url"));
+                                        Toast.makeText(function3.this,jsonobject.optString("url"),Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
     private void iniChoose1()
     {
         num_choose1.setOnValueChangeListener(new OnValueChangeListener() {
             @Override
             public void onValueChanged(int newValue) {
                 size2.setText(String.valueOf(newValue));
+                function3_bean.setW(String.valueOf(newValue));
                 //Toast.makeText(function3.this,"ok"+newValue,Toast.LENGTH_SHORT).show();
             }
         });
@@ -217,6 +262,7 @@ public class function3 extends AppCompatActivity {
             @Override
             public void onValueChanged(int newValue) {
                 margin2.setText(String.valueOf(newValue));
+                function3_bean.setM(String.valueOf(newValue));
                 //Toast.makeText(function3.this,"ok"+newValue,Toast.LENGTH_SHORT).show();
             }
         });
